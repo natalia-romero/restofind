6eaa5227366486acedfabbec37a29b85ef85d2e8
Natalia Romero <natalia.romero_g@mail.udp.cl>
2022-06-06 20:42:06 +0000

    se instala tema de la pagina web

---
+++ database/migrations/2022_05_29_164426_create_evaluations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('evaluations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('restaurant_id')->constrained('restaurants')->onDelete('cascade');
+            $table->text('review');
+            $table->text('score');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('evaluations');
+    }
+};
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin Resto',
+            'email' => 'admin@restofind.com',
+            'description' => 'Un usurario',
+            'city_id' => 1,
+            'email_verified_at' => now(),
+            'password' => Hash::make('123'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+    }
+}
