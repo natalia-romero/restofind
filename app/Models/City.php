82f9e8e9a2b89fcc9200868ef8dc3f5d99cd8923
Natalia Romero <natalia.romero_g@mail.udp.cl>
2022-06-14 23:31:47 -0400

    se corrige modelo de ciudades

--- app/Models/City.php
+++ app/Models/City.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class City extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name'
+    ];
+
+    public function restaurants()
+    {
+        return $this->hasMany(Restaurant::class);
+    }
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
 }
